a3f7a9c287edf43d5893f83da2ceec26881cfb30
Artem <ashikunec@gmail.com>
2024-05-05 17:30:29 +0300

    fit: add daily theme and authorization menu code JS

--- HomePage/script.js
+++ HomePage/script.js
@@ -1,3 +1,4 @@
+
 let loginmenu = document.querySelector('.Login');
 let registrmenu = document.querySelector('.Register');
 let username = localStorage.getItem('username');
@@ -98,6 +99,67 @@ buttoncloselog.addEventListener('click',function(){
   }
 )
 
+//Отправка авторизации
+let pUsername = document.querySelectorAll('.username');
+let nav_third_block = document.getElementById('nav_third-block');
+const Login = document.getElementById('log');
+Login.addEventListener('click', function(){
+  let logup = document.getElementById("Logup").value;
+  let userpassword = document.getElementById("PasswordLog").value;
+  username = logup;
+  password = userpassword;
+  
+ let url = '../JsonFiles/LoginAndPasswords.json'; 
+ fetch(url)
+ .then(response => response.json())
+ .then(result => { for(let element of result){
+  if(element.login == username && element.password == password){
+    logining = true;
+    localStorage.setItem('username',`${username}`);
+    localStorage.setItem('password',`${password}`);
+    localStorage.setItem('logining','true');
+    setLoginNameOnSite();
+    checkerror = false;
+    if(username == 'Admin'){
+      let admindives = document.querySelectorAll('.admin');
+      for(let elem of admindives){
+      elem.style.display = "none";
+      nav_third_block.style.visibility = "visible";
+    }
+    }
+    else{
+      let unauthotized = document.querySelectorAll(".unauthotized");
+      for(let elements of unauthotized){
+        elements.style = "display:flex"
+      }
+    }
+  }
+ 
+ }
+  if(!logining){
+
+    addredblocks();
+  }
+});
+ }
+);
+
+function setLoginNameOnSite(){
+ for(let elem of pUsername){
+  elem.textContent = localStorage.getItem('username');
+  elem.style.display = "block";
+ 
+ }
+ logut.style = "display: flex";
+ loginmenu.style = 'transform: translateX(-2150px)';
+ singup.style.display = 'none';
+ login.style.display = "none";
+ login_burger.style.display = 'none';
+ sign_burger.style.display = 'none';
+
+ burgerlogout.style.display ="flex";
+}
+
 // /Login menu
 
 // Register menu JS
@@ -148,6 +210,92 @@ function loadLanguage(language) {
     ruOption.selected = true;
   }
 // /Language select
+
+// Daily theme
+
+let buttonswitch = document.getElementById("check");
+let box = document.getElementsByClassName('box');
+let text = document.querySelectorAll('p');
+let site_wrapper = document.getElementById('site-wrapper');
+let bg_Content = document.querySelector(".BG_Content");
+let footer_logo = document.getElementById('footer_logo');
+let footer_links = document.getElementsByClassName(".footer_links");
+let footer_block_text = document.getElementById('footer_block-text');
+let footer_block_copyright = document.getElementById('footer_block-copyright');
+//let header = document.querySelector('header');
+// Проверяем, есть ли сохраненное значение в локальном хранилище
+const isDarkThemeEnabled = localStorage.getItem('darkThemeEnabled') === 'true';
+let searchphoto = document.querySelector('.search-photo');
+let basket = document.querySelector('shoping');
+// Устанавливаем начальное значение в соответствии с сохраненным значением
+if (isDarkThemeEnabled) {
+  switchDailytheme();
+  buttonswitch.checked = true;
+  
+}
+function switchDailytheme(){
+  document.body.classList.add("night-theme");
+  //header.classList.add("night-theme"); 
+  //searchicon.style.filter = "invert(1)";
+  //shoppingicon.style.filter = "invert(1)";
+ //login.style.color = "#50FFB1";
+ //login.style.backgroundColor = "#310A31";
+ site_wrapper.style.backgroundColor="#4DA0C9";
+ bg_Content.style.background = "linear-gradient(180deg, rgba(11, 29, 38, 0) 0%, #4DA0C9 61.38%)";
+ footer_logo.style = "filter: invert(1)";
+ //footer_links.style.color = "#0B1D26";
+ footer_block_text.style.color = "#0B1D26";
+ footer_block_copyright.style.color = "#0B1D26";
+
+
+//  for (let elem of text) {
+     
+//     elem.classList.add("night-text");
+//  }
+//  logut.style.backgroundColor = "#310A31";
+//  logut.style.color = "#50FFB1";
+// for(let i = 0; i < box.length; i++){
+//      box[i].classList.add("night-theme");
+// }
+// if (burger_menu_toogle.checked == true){
+//   burger_menu.classList.add("night-theme");
+// }
+}
+function switchDarktheme(){
+  document.body.classList.remove("night-theme");
+  //header.classList.remove("night-theme");
+  //searchicon.style.filter = "invert(0)";
+  //shoppingicon.style.filter = "invert(0)";
+  //logo.style = "filter: invert(0)";
+  site_wrapper.style.backgroundColor="#0B1D26";
+  bg_Content.style.background = "linear-gradient(180deg, rgba(11, 29, 38, 0) 0%, #0B1D26 61.38%)";
+  footer_logo.style = "filter: invert(0)";
+  //footer_links.style.color = "#FFFFFF";
+  footer_block_text.style.color = "#FFFFFF";
+  footer_block_copyright.style.color = "#FFFFFF";
+
+  // for(let elem of box){
+  //     elem.classList.remove("night-theme");
+  // }
+  // for (let elem of text) {
+  //     elem.classList.remove("night-text");
+  // }
+  // if (burger_menu_toogle.checked == true){
+  //   burger_menu.classList.remove("night-theme");
+  // }
+}
+buttonswitch.addEventListener('change',function(){
+    if(buttonswitch.checked){  
+      localStorage.setItem('darkThemeEnabled','true') 
+      switchDailytheme();
+        }
+     else {
+      switchDarktheme();
+         localStorage.setItem('darkThemeEnabled','false')
+     }
+});
+
+// /Daily theme
 
 // Add new account
 
@@ -271,5 +419,35 @@ function validatePassword(password) {
   return null;
 }
 
-// /Add new account
+// /Add new account 
+
+// Slider
+
+let arrowleft = document.getElementById("arrow-left");
+let arrowright = document.getElementById("arrow-right");
+let slider = document.querySelector(".Slider-container");
+slider.classList.add='.slide-transition'
+let slideWidth = 5;
+let a = 0;
+console.log(document.documentElement.clientWidth)
+
+arrowright.addEventListener('click', function () {
+    if (a < slideWidth - 1) {
+        a++;
+    } else {
+        a = 0;
+    }
+    slider.style.transform = `translateX(-${a*21}%)`;
+});
+
+arrowleft.addEventListener('click', function () {
+    if (a > 0) {
+        a--;
+    } else {
+        a = slideWidth - 1;
+    }
+    slider.style.transform = `translateX(-${a*21}%)`;
+});
+
+// /Slider
 
